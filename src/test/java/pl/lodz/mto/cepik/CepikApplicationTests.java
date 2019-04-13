cb7b10e4f69c45b85fd32ef46f4b80f8ad22145a
Radosław Kępa <204202@edu.p.lodz.pl>
2019-04-13 17:43:15 +0200

    initial

---
+++ src/test/java/pl/lodz/mto/cepik/CepikApplicationTests.java
@@ -0,0 +1,18 @@
+package pl.lodz.mto.cepik;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootConfiguration
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CepikApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
